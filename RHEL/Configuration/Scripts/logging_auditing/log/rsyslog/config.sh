c96ad75af3df57cf6a77202189f515c382049869
IoanOik <johnoikonomou2001@gmail.com>
2024-09-03 18:51:06 +0300

    Initial release

--- RHEL/Configuration/Scripts/logging_auditing/log/rsyslog/config.sh
+++ RHEL/Configuration/Scripts/logging_auditing/log/rsyslog/config.sh
@@ -19,8 +19,8 @@ rpm -q rsyslog
 {
     printf "%s\n" '$FileCreateMode 0640'
 
-    # Ensure rsyslog is configured to send logs to a remote log
-    printf "%s\n" '*.info action(type="omfwd" target="172.18.255.153" port="514" protocol="udp" action.resumeRetryCount="100" queue.type="LinkedList" queue.size="1000")'
+    # Ensure rsyslog is configured to send logs to a remote log server
+    printf "%s\n" '*.info action(type="omfwd" target="1.1.1.1" port="514" protocol="udp" action.resumeRetryCount="100" queue.type="LinkedList" queue.size="1000")'
 
     # Ensure rsyslog listens on dev/log domain socket for any log  messages
     # from journald or auditd
